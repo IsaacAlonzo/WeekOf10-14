5f2bf72bc679cfb3591d37a4068a13bbe283a858
IsaacAlonzo <isaac.alonzo@icloud.com>
2019-10-15 15:32:47 -0500

    try #1

--- WeekOf10/14.py
+++ WeekOf10/14.py
@@ -43,5 +43,20 @@ print(acc)
 
 # acc was 0 the added plus 1 then made acc = 1 and repeated process
 
+# Compute the sum of the first 100 even numbers
 
-
+acc = 0
+for val in range(2, 100):
+    acc = acc + val
+print(acc)
+# Compute the sum of the first 50 odd numbers
+# Compute the average of the first 100 odd numbers
+# Write a function that returns the average of the first N numbers, where
+#   N is a parameter
+# Write a function called factorial that computes the product of the first N
+#   numbers, where N is a parameter
+# Each number in the Fibonacci sequence is the sum of the previous two numbers.
+#   The first two numbers in the sequence are 1 and 1.  Compute the 10th
+#   Fibonacci number.
+# Write a function to compute the Nth Fibonacci number, where N is a parameter.
+#   You may assume that N will be greater than or equal to 3.
